d21f62eb2b8ff62d83961cc60b950d24927e0cc1
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-04-10 21:25:43 +0300

    preparation continued

--- src/index.js
+++ src/index.js
@@ -7,3 +7,6 @@ const DEBOUNCE_DELAY = 300;
 
 const country = FetchServise.fetchCountries('Ukraine');
 console.log(country);
+
+Notify.info('Too many matches found. Please enter a more specific name.');
+Notify.failure('Oops, there is no country with that name');
