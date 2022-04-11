533d84ccee9d6d0a6f956c0c5cc843af3dd5e971
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-04-11 13:50:42 +0300

    fetchCoutries function manufactured

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,19 @@
+const URL_COUNTRIES = 'https://restcountries.com/v3.1';
+const filter = new URLSearchParams({
+  fields: `name,capital,population,flags,languages`,
+});
+
 function fetchCountries(name) {
-  return name;
+  const path = `/name/${name}`;
+  const url = URL_COUNTRIES + path + `?${filter}`;
+  const countries = fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+
+  return countries;
 }
 
 export { fetchCountries };
